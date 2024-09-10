960b2f69fdc9bea9460137f302796990154071b6
Zaripov Rinat <eslimbos@gmail.com>
2024-09-10 16:51:25 +0300

    final

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,9 +7,10 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
-        Cat bean2 = applicationContext.getBean("cat", Cat.class);
-        Cat bean3 = applicationContext.getBean("cat", Cat.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.equals(bean1));
         System.out.println(bean2.equals(bean3));
         System.out.println(bean.getMessage());
